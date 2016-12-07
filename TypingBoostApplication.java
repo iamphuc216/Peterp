9d4d666c498d41242d993025ae95ae552c4ec74e
iamphuc216 <iamphuc216@gmail.com>
2016-12-07 18:44:25 +1100

    Add files via upload

---
+++ TypingBoostApplication.java
@@ -0,0 +1,37 @@
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import model.Test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Thutan
+ */
+public class TypingBoostApplication extends Application {
+   public static void main(String args[]) {
+   launch(args);
+   }
+    
+   @Override
+   public void start(Stage stage) throws Exception {
+       FXMLLoader loader = new FXMLLoader(getClass().getResource("view/typingwindow.fxml"));
+       Parent root = loader.load();
+       stage.setTitle("Typing Window");
+       stage.setScene(new Scene(root));
+       stage.sizeToScene();
+       stage.show();
+       
+       
+       
+       
+   }
+}
